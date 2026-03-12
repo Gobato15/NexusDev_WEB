0b1fb1d21357b6ef2c76e267e2ca23244adca3c4
Luis Felipe <luisfelipemathiasleite@gmail.com>
2026-03-11 22:04:20 -0300

    Criação da compra e venda, com index e atualizar.php

---
+++ Objetos/compra.php
@@ -0,0 +1,94 @@
+<?php
+
+class Compra
+{
+    public $NotaFiscal_Entrada;
+    public $Valor_Total;
+    public $Data_Compra;
+    public $CPF;
+    public $CNPJ_Lab;
+    public $bd;
+
+    public function __construct($bd)
+    {
+        $this->bd = $bd;
+    }
+
+    public function lerTodos()
+    {
+        $sql = "SELECT * FROM compra";
+        $resultado = $this->bd->query($sql);
+
+        return $resultado->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function pesquisarCompra($NotaFiscal_Entrada)
+    {
+        $sql = "SELECT * FROM compra WHERE NotaFiscal_Entrada = :NotaFiscal_Entrada";
+        $resultado = $this->bd->prepare($sql);
+        $resultado->bindParam(":NotaFiscal_Entrada", $NotaFiscal_Entrada);
+        $resultado->execute();
+
+        return $resultado->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function cadastrar()
+    {
+        $sql = "INSERT INTO compra (Valor_Total, Data_Compra, CPF, CNPJ_Lab)
+                VALUES (:Valor_Total, :Data_Compra, :CPF, :CNPJ_Lab)";
+
+        $stmt = $this->bd->prepare($sql);
+
+        $stmt->bindParam(":Valor_Total", $this->Valor_Total);
+        $stmt->bindParam(":Data_Compra", $this->Data_Compra, PDO::PARAM_STR);
+        $stmt->bindParam(":CPF", $this->CPF, PDO::PARAM_STR);
+        $stmt->bindParam(":CNPJ_Lab", $this->CNPJ_Lab, PDO::PARAM_STR);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function excluir()
+    {
+        $sql = "DELETE FROM compra WHERE NotaFiscal_Entrada = :NotaFiscal_Entrada";
+        $stmt = $this->bd->prepare($sql);
+        $stmt->bindParam(":NotaFiscal_Entrada", $this->NotaFiscal_Entrada, PDO::PARAM_INT);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function atualizar(){
+        $sql = "UPDATE compra SET Valor_Total = :Valor_Total, Data_Compra = :Data_Compra, CPF = :CPF, CNPJ_Lab = :CNPJ_Lab WHERE NotaFiscal_Entrada = :NotaFiscal_Entrada";
+
+        $stmt = $this->bd->prepare($sql);
+
+        $stmt->bindParam(":Valor_Total", $this->Valor_Total);
+        $stmt->bindParam(":Data_Compra", $this->Data_Compra, PDO::PARAM_STR);
+        $stmt->bindParam(":CPF", $this->CPF, PDO::PARAM_STR);
+        $stmt->bindParam(":CNPJ_Lab", $this->CNPJ_Lab, PDO::PARAM_STR);
+        $stmt->bindParam(":NotaFiscal_Entrada", $this->NotaFiscal_Entrada, PDO::PARAM_INT);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function buscarCompra($NotaFiscal_Entrada)
+    {
+        $sql = "SELECT * FROM compra WHERE NotaFiscal_Entrada = :NotaFiscal_Entrada";
+        $resultado = $this->bd->prepare($sql);
+        $resultado->bindParam(":NotaFiscal_Entrada", $NotaFiscal_Entrada);
+        $resultado->execute();
+
+        return $resultado->fetch(PDO::FETCH_OBJ);
+    }
+}
---
+++ Objetos/venda.php
@@ -0,0 +1,89 @@
+<?php
+
+Class venda{
+    public $NotaFiscal_Saida;
+    public $Data_Venda;
+    public $Valor_Venda;
+    public $CNPJ_Drog;
+    public $CPF;
+    public $bd;
+
+    public function __construct($bd){
+        $this->bd = $bd;
+    }
+    public function lerTodos(){
+        $sql = "SELECT * FROM venda";
+        $resultado = $this->bd->query($sql);
+        $resultado->execute();
+
+        return $resultado->fetchAll(PDO::FETCH_OBJ);
+    }
+    public function pesquisarVenda($NotaFiscal_Saida){
+        $sql = "SELECT * FROM venda WHERE NotaFiscal_Saida = :NotaFiscal_Saida";
+        $resultado = $this->bd->prepare($sql);
+        $resultado->bindParam(":NotaFiscal_Saida", $NotaFiscal_Saida);
+        $resultado->execute();
+
+        return $resultado->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function cadastrar(){
+        $sql = "INSERT INTO venda (Data_Venda, Valor_Venda, CNPJ_Drog, CPF) 
+            VALUES (:Data_Venda, :Valor_Venda, :CNPJ_Drog, :CPF)";
+
+        $stmt = $this->bd->prepare($sql);
+
+        $stmt->bindParam(":Data_Venda", $this->Data_Venda, PDO::PARAM_STR);
+        $stmt->bindParam(":Valor_Venda", $this->Valor_Venda);
+        $stmt->bindParam(":CNPJ_Drog", $this->CNPJ_Drog, PDO::PARAM_STR);
+        $stmt->bindParam(":CPF", $this->CPF, PDO::PARAM_STR);
+
+        if($stmt->execute()){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public function excluir()
+    {
+        $sql = "DELETE FROM venda WHERE NotaFiscal_Saida = :NotaFiscal_Saida";
+        $stmt = $this->bd->prepare($sql);
+        $stmt->bindParam(":NotaFiscal_Saida", $this->NotaFiscal_Saida, PDO::PARAM_INT);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function atualizar()
+    {
+        $sql = "UPDATE venda SET Data_Venda = :Data_Venda, Valor_Venda = :Valor_Venda, CNPJ_Drog = :CNPJ_Drog, CPF = :CPF WHERE NotaFiscal_Saida = :NotaFiscal_Saida";
+
+        $stmt = $this->bd->prepare($sql);
+
+        $stmt->bindParam(":Data_Venda", $this->Data_Venda, PDO::PARAM_STR);
+        $stmt->bindParam(":Valor_Venda", $this->Valor_Venda);
+        $stmt->bindParam(":CNPJ_Drog", $this->CNPJ_Drog, PDO::PARAM_STR);
+        $stmt->bindParam(":CPF", $this->CPF, PDO::PARAM_STR);
+        $stmt->bindParam(":NotaFiscal_Saida", $this->NotaFiscal_Saida, PDO::PARAM_INT);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function buscaVenda($NotaFiscal_Saida)
+    {
+        $sql = "SELECT * FROM venda WHERE NotaFiscal_Saida = :NotaFiscal_Saida";
+        $resultado = $this->bd->prepare($sql);
+        $resultado->bindParam(":NotaFiscal_Saida", $NotaFiscal_Saida);
+        $resultado->execute();
+
+        return $resultado->fetch(PDO::FETCH_OBJ);
+    }
+}
